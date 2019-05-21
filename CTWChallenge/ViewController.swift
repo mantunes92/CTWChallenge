9ba095b71d1a92250c4c3adbac92393d68dfbbe3
Marcelo <m.filipe.a@gmail.com>
2019-05-21 21:33:37 +0100

    - Created project; - Updated .gitignore to exclude carthage checkouts

---
+++ CTWChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CTWChallenge
+//
+//  Created by Marcelo Antunes on 5/21/19.
+//  Copyright © 2019 Marcelo Antunes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
